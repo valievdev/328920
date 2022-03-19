d10189fa1f7fb5dc8e600a48fb75803b635cb2fb
azat valiev <azvaliev@gmail.com>
2022-03-19 13:27:48 -0700

    Login page completed. Seperating into component

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import {
   Grid,
   Box,
@@ -7,10 +7,66 @@ import {
   Button,
   FormControl,
   TextField,
-} from '@material-ui/core';
+  Paper,
+  InputAdornment
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles"
+import bgLanguage from "./img/bg-img.png"
+import chatBubble from "./img/bubble.svg"
+
+const useSidebarStyles = makeStyles({
+  aboutDiv: {
+    position: "absolute",
+    top: "30%",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    zIndex: 10,
+  },
+  aboutText: {
+    marginTop: '5%',
+    color: 'white',
+    fontSize: '1.8rem'
+  },
+  overlay : {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+  },
+  image : {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover"
+  }
+})
+const useLoginStyles = makeStyles({
+  createAcc: {
+    display: "flex", 
+    flexDirection: "row",
+    width: "95%",
+    margin: "2vh 5% 0 auto",
+    alignItems: "center"
+  },
+  createAccText: {
+    marginRight: "4vw",
+    marginLeft: "auto",
+  },
+  paper: {
+    minWidth: "13vw",
+    width: "fit-content",
+    display: "flex"
+  },
+  welcomeText: {
+    margin: "0 auto 0 0"
+  }
+})
 
 const Login = ({ user, login }) => {
   const history = useHistory();
+  const sidebarStyles = useSidebarStyles();
+  const loginStyles = useLoginStyles();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -23,47 +79,83 @@ const Login = ({ user, login }) => {
   };
 
   useEffect(() => {
-    if (user && user.id) history.push('/home');
+    if (user && user.id) history.push("/home");
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
+    <Box display="grid" height="100vh" sx={{display: "flex", flexDirection: "row", boxSizing: "border-box"}}>
+      <Grid item sm={6} md={5} lg={5} xl={4} >
+        <Box sx={{position: "relative", display: {xs: "none", sm: "flex"}}} width="100%" height="100%">
+          <Box className={sidebarStyles.aboutDiv}>
+            <Box component="img" src={chatBubble} />
+            <Typography variant="h6" component="h5" align="center" className={sidebarStyles.aboutText}>
+              Converse with anyone<br/>with any language
+            </Typography>
+
+          </Box>
+          <Box sx={{bgcolor: "primary.overlay"}} className={sidebarStyles.overlay}/>
+          <Box component="img" src={bgLanguage} className={sidebarStyles.image} sx={{objectFit: "cover"}} width="inherit" height="100%"/>
+        </Box>
+      </Grid>
+      <Grid container item xs={12} sm={6} md={7} lg={7} xl={8} direction="column">
+        <Box className={loginStyles.createAcc}>
+          <Typography variant="subtitle1" className={loginStyles.createAccText}>
+            Don't have an account?
+          </Typography>
           <Link href="/register" to="/register">
-            <Button>Register</Button>
+            <Paper elevation={3}>
+                <Typography variant="button" color="primary">
+                  Create Account
+                </Typography>
+            </Paper>
           </Link>
-        </Grid>
+        </Box>
+        <Box mt={"20vh"} mx={"auto"} mb={"auto"} sx={{width: "40%"}}>
         <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
+          <Grid container justifyContent="center" direction="column">
+            <Typography variant="h4" component="h1" align="left" className={loginStyles.welcomeText}>
+              Welcome Back!
+            </Typography>
               <FormControl margin="normal" required>
                 <TextField
                   aria-label="username"
                   label="Username"
                   name="username"
                   type="text"
+                  size="large"
                 />
               </FormControl>
-            </Grid>
             <FormControl margin="normal" required>
               <TextField
-                label="password"
+                label="Password"
                 aria-label="password"
                 type="password"
                 name="password"
+                size="large"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <Box ml={2} mt={.5} color="primary.main" fontSize={"0.8rem"} fontWeight={600}>
+                        Forgot?</Box>
+                    </InputAdornment>
+                  ),
+                }}
               />
             </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
+            <Box mt={"8vh"} display="flex">
+              <Button type="submit" variant="contained" size="small" color="primary">
+                <Typography variant="button" >
+                  <Box fontWeight={700}> 
                 Login
+                  </Box>
+                </Typography>
               </Button>
-            </Grid>
+            </Box>
           </Grid>
         </form>
-      </Box>
-    </Grid>
+        </Box>
+      </Grid>
+    </Box>
   );
 };
 
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,24 +1,71 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+import { alpha } from "@material-ui/core/styles/colorManipulator"
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
   typography: {
     fontFamily: "Open Sans, sans-serif",
     fontSize: 14,
+    h4: {
+      fontWeight: "600",
+      fontSize: "1.8rem",
+    },
     button: {
       textTransform: "none",
       letterSpacing: 0,
-      fontWeight: "bold"
-    }
+      fontWeight: "600",
+      fontSize: "1.05rem",
+      textDecoration: "none !important",
+      margin: "auto",
+      display: "inline-block",
+      padding: "1.8vh 2.5vw",
+      color: "white",
+      borderRadius: "4px"
+    },
+    subtitle1: {
+      fontSize: "1rem",
+      color: "#B0B0B0",
+    },
   },
   overrides: {
     MuiInput: {
       input: {
-        fontWeight: "bold"
+        fontWeight: "bold",
+      },
+      underline : {
+        "&:before": {
+          borderBottomColor: "#D5DFED" 
+        }
+      }
+    },
+    MuiInputBase: {
+      borderBottomWidth: "0.5px",
+    },
+    MuiPaper: {
+      elevation3 : {
+        boxShadow: "-1px 5px 3px -2px rgba(0, 0, 0, 0.05),0px 3px 4px 0px rgba(0,0,0,0.05),0px 1px 8px 0px rgba(0,0,0,0.05)",
+      }
+    },
+    MuiButton: {
+      root: {
+        borderRadius: 3
+      },
+      containedSizeSmall: {
+        padding: "0 2vw"
       }
     }
+
   },
   palette: {
-    primary: { main: "#3A8DFF" },
-    secondary: { main: "#B0B0B0" }
-  }
+    primary: { main: "#3A8DFF", overlay: alpha("#5BA0FF", 0.85) }, 
+    secondary: { main: "#B0B0B0" },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 900,
+      lg: 1200,
+      xl: 1400,
+    },
+  },
 });
